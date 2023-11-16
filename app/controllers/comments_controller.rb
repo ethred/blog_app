7241a8e85bd3aca3f9e744c814ef77b578a39827
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-11-16 10:35:59 +0100

    Add destroy for comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,17 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to user_post_path(@user, @post), notice: 'Comment was successfully deleted.'
+    else
+      redirect_to user_post_path(@user, @post), alert: 'Failed to delete comment.'
+    end
+  end
+
   private
 
   def comment_params
