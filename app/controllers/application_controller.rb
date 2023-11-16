1b58a88441ede4ee21864d89d5630a19bfdcb79b
redwan <redtah2007@yahoo.com>
2023-11-16 12:12:59 +0300

    add authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,26 @@
 # class ApplicationController < ActionController::Base
-#   # def current_user
-#   #   @current_user ||= User.includes(posts: %i[comments likes]).first
-#   # end
+#   protect_from_forgery with: :exception
+
+#   before_action :authenticate_user!
+
+#   before_action :update_allowed_parameters, if: :devise_controller?
+
+#   protected
+
+#   def after_sign_in_path_for(_resource)
+#     users_path
+#   end
+
+#   def update_allowed_parameters
+#     devise_parameter_sanitizer.permit(:sign_up) do |u|
+#       u.permit(:name, :email, :bio, :photo, :password, :password_confirmation)
+#     end
+#     devise_parameter_sanitizer.permit(:account_update) do |u|
+#       u.permit(:name, :email, :bio, :photo, :password, :current_password)
+#     end
+#   end
 # end
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
@@ -12,8 +30,8 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(_resource)
-    users_path
+  def after_sign_in_path_for(resource)
+    user_path(resource) # Redirect to the signed-in user's profile page
   end
 
   def update_allowed_parameters
@@ -25,20 +43,3 @@ class ApplicationController < ActionController::Base
     end
   end
 end
-
-# class ApplicationController < ActionController::Base
-#   include CanCan::ControllerAdditions
-#   protect_from_forgery with: :exception
-#   before_action :authenticate_user!
-#   before_action :configure_permitted_parameters, if: :devise_controller?
-
-#   protected
-
-#   def configure_permitted_parameters
-#     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
-#     devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
-#   end
-
-#   helper_method :current_user
-# end
-# letter_opener
